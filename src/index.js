960d3254a09e54396c93d101dd136140676f0817
ivanberry <tabvim.dev@gmail.com>
2018-03-28 14:41:17 +0800

    tree shaking but failed to shaking?

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import printMe from './print.js';
+import './style.css';
+import {cube} from './math.js';
 
 function component() {
-  let el = document.createElement('div');
-  let button = document.createElement('button');
+  let el = document.createElement('pre');
+  //let button = document.createElement('button');
 
-  el.innerHTML = ['Hello webpack', '5 cubed reuslt is' ].join('\n');
-
-  button.innerText = 'Ahah';
-  button.onclick = printMe;
-  el.appendChild(button);
+  //el.innerHTML = ['Hello webpack', '5 cubed reuslt is' ].join('\n');
+  el.innerHTML = ['Tree Sharking', cube(5)].join('\n');
+  //button.innerText = 'Click!';
+  //button.onclick = printMe;
+  //el.appendChild(button);
   return el;
+
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
